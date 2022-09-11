33c16192251b16de4094c71d0a089836faa02b16
Ramya Sachin <ramyasachin@Ramyas-MacBook-Air.local>
2022-09-11 17:48:32 +0530

    Added SampleApiProperties page to the framework

---
+++ src/main/java/properties/SampleApiProperties.java
@@ -0,0 +1,6 @@
+package properties;
+
+public class SampleApiProperties {
+    public static final PropertiesReader propertiesReader = new PropertiesReader();
+    public static final String baseUrl = propertiesReader.getUrl();
+}
